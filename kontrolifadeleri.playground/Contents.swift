86d5387b5db8825e18cff997e1cfd51775589139
erolerme <erolerme@gmail.com>
2019-01-27 20:47:30 +0300

    Kontrol Yapıları

---
+++ kontrolifadeleri.playground/Contents.swift
@@ -0,0 +1,27 @@
+/*
+ 
+ Kontrol İfadeleri ~ if else yapısı
+ 
+ */
+
+//if else
+
+if 21 < 35 {
+    print("21 küçüktür 35'ten")
+}
+
+
+
+//-----
+
+let ahmetinYasi:Int = 41
+let mehmetinYasi:Int = 36
+
+if ahmetinYasi == mehmetinYasi {
+    print("Ahmet'in yaşı Mehmet'in yaşınıa eşittir.")
+}else if ahmetinYasi < mehmetinYasi {
+    print("Ahmet'in yaşı küçüktür Mehmet'in yaşından.")
+}else{
+    print("Ahmet'in yaşı büyüktür Mehmetin yaşından.")
+}
+
